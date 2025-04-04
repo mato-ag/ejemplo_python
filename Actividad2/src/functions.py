18e1f2294bc13636805f9fffddc29644333d7b05
Mato Pavon Tolosa <matopavontolosa@gmail.com>
2025-04-04 10:38:57 -0300

    Agrego carpeta src y funciones

---
+++ Actividad2/src/functions.py
@@ -0,0 +1,15 @@
+
+def sumar_puntaje(estadisticas):
+    total = estadisticas['kills'] * 3 + estadisticas['assists']
+    if estadisticas['deaths']:
+        total = total - 1
+    return total
+
+def calcular_mvp(mvp):
+    return max(mvp[0].items(), key=lambda item: item[1]['puntos'])[0]
+
+def actualizar_datos(mvp, jugador, estadisticas):
+    mvp[0][jugador]['kills'] = mvp[0][jugador]['kills'] + estadisticas['kills']
+    mvp[0][jugador]['assists'] = mvp[0][jugador]['assists'] + estadisticas['assists']
+    if estadisticas['deaths']:
+        mvp[0][jugador]['deaths'] += 1
